d60e0b223863b760d5566488acc0a19a3e138bf8
Sean Robenalt <srob0722@gmail.com>
2024-06-05 06:09:29 -0400

    font edits

--- pages/about.js
+++ pages/about.js
@@ -30,7 +30,7 @@ export default function About() {
           <h1 style={{ color: "#C9362B", fontFamily: "SF Fedora" }}>Dig</h1>
         </div>
 
-        <div style={{ fontFamily: "LaBeouf" }}>
+        <div style={{ fontFamily: "Livvic" }}>
           <div style={{ margin: "10px" }}>
             <Image
               src="/grail.png"
@@ -40,29 +40,44 @@ export default function About() {
               style={{ margin: "10px" }}
             />
 
-            <h2 style={{ margin: "5px" }}>
-              The{" "}
-              <span style={{ color: "#C9362B", fontFamily: "SF Fedora" }}>
-                dig
-              </span>{" "}
-              NFT is an NFT on the Degen chain.
-            </h2>
-            <h2 style={{ margin: "5px" }}>
-              Seize it by clicking the frame on any FC client (coming soon).
-            </h2>
-            <h2 style={{ margin: "5px" }}>
-              There will also be a contract deployed to Base, that stores
-              ERC20s.
-            </h2>
-            <h2 style={{ margin: "5px" }}>
-              At a random time each week, this contract will transfer 1k of a
-              random ERC20 in the contract to whoever is the{" "}
-              <span style={{ color: "#C9362B", fontFamily: "SF Fedora" }}>
-                dig
-              </span>{" "}
-              NFT holder. Check back to this page once the game is live to see
-              the current balance of ERC20s in the contract.
-            </h2>
+            <div style={{ margin: "10px", maxWidth: "600px" }}>
+              <h2 style={{ margin: "10px" }}>
+                The
+                <span
+                  style={{
+                    color: "#C9362B",
+                    fontFamily: "SF Fedora",
+                    margin: "0px 10px",
+                  }}
+                >
+                  dig
+                </span>
+                NFT is an NFT on the Degen chain.
+              </h2>
+              <h2 style={{ margin: "10px" }}>
+                Seize it by clicking the frame on any Farcaster client (coming
+                soon).
+              </h2>
+              <h2 style={{ margin: "10px" }}>
+                There will also be a contract deployed to Base, that stores
+                ERC20s.
+              </h2>
+              <h2 style={{ margin: "10px" }}>
+                At a random time each week, this contract will transfer 1k of a
+                random ERC20 in the contract to whoever is the
+                <span
+                  style={{
+                    color: "#C9362B",
+                    fontFamily: "SF Fedora",
+                    margin: "0px 10px",
+                  }}
+                >
+                  dig
+                </span>
+                NFT holder. Check back to this page once the game is live to see
+                the current balance of ERC20s in the contract.
+              </h2>
+            </div>
           </div>
         </div>
       </main>
