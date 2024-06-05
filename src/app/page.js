7c71a405dbbd191c8a98eee154cfd0a271968879
Sean Robenalt <srob0722@gmail.com>
2024-06-04 21:59:36 -0400

    image edits

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -5,6 +6,22 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <h1 style={{ color: "#C9362B" }}>Dig</h1>
+      </div>
+
+      <div style={{ fontFamily: "LaBeouf" }}>
+        <div style={{ margin: "10px" }}>
+          <h2>coming soon.</h2>
+
+          <Image
+            src="/grail.png"
+            alt="dig"
+            width={250}
+            height={250}
+            style={{ margin: "10px" }}
+          />
+        </div>
+
+        <hr />
       </div>
     </main>
   );
