487e0329e980fdaec1e5926bf018994cb020fcbc
Sean Robenalt <srob0722@gmail.com>
2024-06-04 22:31:58 -0400

    couple more minor edits

--- api/hiw/read5.js
+++ api/hiw/read5.js
@@ -6,6 +6,9 @@ export default async function handler(req, res) {
           <head>
             <meta property="fc:frame" content="vNext" />
             <meta property="fc:frame:image" content="https://www.dig.bingo/hiw6.png" />
+            <meta property="fc:frame:button:1" content="View the Website" />
+            <meta property="fc:frame:button:1:action" content="link" />
+            <meta property="fc:frame:button:1:target" content="https://www.dig.bingo/about" />
             <meta property="fc:frame:image:aspect_ratio" content=" 1.91:1" />
           </head>
         </html>
--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function About() {
   return (
@@ -30,7 +31,37 @@ export default function About() {
 
         <div style={{ fontFamily: "LaBeouf" }}>
           <div style={{ margin: "10px" }}>
-            <h2>coming soon.</h2>
+            <Image
+              src="/grail.png"
+              alt="dig"
+              width={250}
+              height={250}
+              style={{ margin: "10px" }}
+            />
+
+            <h2 style={{ margin: "5px" }}>
+              The{" "}
+              <span style={{ color: "#C9362B", fontFamily: "SF Fedora" }}>
+                dig
+              </span>{" "}
+              NFT is an NFT on the Degen chain.
+            </h2>
+            <h2 style={{ margin: "5px" }}>
+              Seize it by clicking the frame on any FC client (coming soon).
+            </h2>
+            <h2 style={{ margin: "5px" }}>
+              There will also be a contract deployed to Base, that stores
+              ERC20s.
+            </h2>
+            <h2 style={{ margin: "5px" }}>
+              At a random time each week, this contract will transfer 1k of a
+              random ERC20 in the contract to whoever is the{" "}
+              <span style={{ color: "#C9362B", fontFamily: "SF Fedora" }}>
+                dig
+              </span>{" "}
+              NFT holder. Check back to this page once the game is live to see
+              the current balance of ERC20s in the contract.
+            </h2>
           </div>
         </div>
       </main>
