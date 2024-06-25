b0fafb961081c4cc345515b06908fdb62c4051de
Sean Robenalt <srob0722@gmail.com>
2024-06-25 16:15:20 -0400

    use pinata for fid

--- pages/api/leader.js
+++ pages/api/leader.js
@@ -2,10 +2,46 @@ import { createCanvas, loadImage } from "@napi-rs/canvas";
 import path from "path";
 import { uploadImageToImgur } from "../../utils/uploadImage";
 import { getLeaders } from "../../utils/dig";
+import { PinataFDK } from "pinata-fdk";
+
+const fdk = new PinataFDK({
+  pinata_jwt: process.env.PINATA_JWT,
+  pinata_gateway: process.env.PINATA_GATEWAY,
+});
 
 export default async (req, res) => {
   if (req.method === "POST") {
+    const { isValid, message } = await fdk.validateFrameMessage(req.body);
+
+    if (!isValid) {
+      return res.status(200).send(`
+          <!DOCTYPE html>
+            <html>
+              <head>
+                <meta property="fc:frame" content="vNext" />
+                <meta property="fc:frame:image" content="https://www.dig.bingo/lbError.png" />
+                <meta property="og:image" content="https://www.dig.bingo/lbError.png" />
+                <meta property="fc:frame:button:1" content="Dig" />
+                <meta property="fc:frame:button:1:action" content="post" />
+                <meta
+                  property="fc:frame:button:1:post_url"
+                  content="https://www.dig.bingo/api/leaderDig"
+                />
+                <meta property="fc:frame:button:2" content="My Stats" />
+                <meta property="fc:frame:button:2:action" content="post" />
+                <meta
+                  property="fc:frame:button:2:post_url"
+                  content="https://www.dig.bingo/api/myStats"
+                />
+                <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
+              </head>
+            </html>
+        `);
+    }
+
     try {
+      const { fid } = message.data;
+
       const imgPath = path.join(process.cwd(), "public", "lb.png");
       const img = await loadImage(imgPath);
 
@@ -18,8 +54,6 @@ export default async (req, res) => {
 
       const leaders = await getLeaders();
 
-      console.log("leaders", leaders);
-
       const headerHeight = 230;
       const startY = headerHeight + 50;
 
@@ -27,24 +61,18 @@ export default async (req, res) => {
         const textX = 110;
         const y = startY + index * (32 + 10);
 
-        ctx.fillStyle = "#000000";
+        ctx.fillStyle = fid.toString() === leader[0] ? "#347d2e" : "#000000";
         const text = `${index + 1}. ${leader[0]}`;
-        console.log("text", text);
         ctx.fillText(text, textX, y);
 
-        ctx.fillStyle = "#C9362B";
+        ctx.fillStyle = "#347d2e";
         const cnt = `${leader[1]}`;
-        console.log("cnt", cnt);
         ctx.fillText(cnt, 380, y);
       });
-
-      console.log("canvas", canvas);
 
       const buffer = canvas.toBuffer("image/png");
 
       const imgurResponse = await uploadImageToImgur(buffer);
-
-      console.log("imgurResponse", imgurResponse);
 
       return res.status(200).send(`
         <!DOCTYPE html>
